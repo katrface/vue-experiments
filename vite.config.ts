f420abbcf72975e8d02aeec52e62fe3042e9b0f7
katrface <majdanov.dev@gmail.com>
2024-04-13 14:50:21 +0300

    lint

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import eslint from 'vite-plugin-eslint'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['src/app/assets/*'],
-  plugins: [vue(), vuetify(), VueDevTools()],
+  plugins: [vue(), vuetify(), VueDevTools(), eslint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
